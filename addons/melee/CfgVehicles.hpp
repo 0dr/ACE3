7a8e4f9f270f43eda8e29310cdb9d57a3874d566
Th3Cr0w <th3cr0w@gmx.net>
2018-02-14 15:06:47 +0100

    fixed typo  added conditions

--- addons/melee/CfgVehicles.hpp
+++ addons/melee/CfgVehicles.hpp
@@ -4,14 +4,14 @@ class CfgVehicles {
         class ACE_Actions {
             class ACE_MainActions {
                 class GVAR(takedown) {
-                    displayName = CSTRING(takdown);
-                    condition = QUOTE(true);
-                    statement = QUOTE([_player, _target] call FUNC(takedown));
+                    displayName = CSTRING(takedown);
+                    condition = QUOTE(isAlive _target && [_target] call EFUNC(common,isAwake));
+                    statement = QUOTE([ARR2(_player, _target)] call FUNC(takedown));
                     distance = 2;
                     showDisabled = 0;
                     priority = 2;
                     icon = ""; //QPATHTOF(UI\ATRAG_Icon.paa);
-                    exceptions[] = {};
+                    exceptions[] = {"isNotSwimming"};
                 };
             };
         };
